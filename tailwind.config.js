6e22611ff8cea67e40d85a78f23b1b53bdcd831d
norman-andrians <norman25.na@gmail.com>
2023-12-05 17:26:16 +0700

    segini aja dulu bisi mati lampu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#3A10E5',
-        'primary-bg': '#F0F3FF'
+        'primary-bg': '#F0F3FF',
+        'tertiary': '#707070'
+      },
+      fontFamily: {
+        'dm-sans': 'DM Sans'
       }
     },
   },
