f07c2f41f336ad52d08c88d241ddcddde998191d
norman-andrians <norman25.na@gmail.com>
2023-12-05 17:06:17 +0700

    inisialisasi project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3A10E5',
+        'primary-bg': '#F0F3FF'
+      }
+    },
+  },
+  plugins: [],
+}
+
